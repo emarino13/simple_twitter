fe55499b8ad407fc57b73d5b790173a092541d95
Elaine Marino <elaine.c.marino@gmail.com>
2012-06-21 16:11:50 -0600

    Simple Twitter Homework 6_21

--- lib/simple_twitter/search.rb
+++ lib/simple_twitter/search.rb
@@ -5,21 +5,24 @@ module SimpleTwitter
     SEARCH_URL = "http://search.twitter.com/search.json"
 
     attr_accessor(:per_page)
+    attr_accessor(:lang)
 
     def initialize
       @per_page=10
+      @lang = 'en'
     end
 
     def search (query)
       params = {
         :q => query,
-        :rpp => @per_page,
+        :rpp => @per_page, :lang => @lang
       }
       response = HTTParty.get(SEARCH_URL, :query => params)
       #same as {:query=>params}    end
       #:timeout is a key inside the hash that works with HTTParty
       JSON.parse(response.body)
       #same as return.JSON.parse(response.body) -- last line is instructions
+    end
   end
 end
 
--- test/search_test.rb
+++ test/search_test.rb
@@ -1,13 +1,19 @@
 require ('test/unit')
 require ('mocha')
 require ('httparty')
-require (json_file)
+require ('simple_twitter.rb')
+
 
 class MockExampleTest < Test::Unit::TestCase
   def test_example_mocking
 
-    file_name = File.expand_path('../json/gaga.json'_FILE_)
+    s=SimpleTwitter::Search.new
+    file_name = File.expand_path('../test/json_file.json', File.dirname(__FILE__))
     response = stub('first test ever', :body => File.read(file_name))
     HTTParty.expects(:get).returns(response)
-  end
+    #assert_equal(response, HTTParty.get('http://search.twitter.com/search.json?q=(gaga&rpp=1'))
+
+    tweets=s.search("gaga")
+    assert(tweets.is_a?(Hash))
+ end
 end
